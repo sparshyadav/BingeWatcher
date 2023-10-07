81105981e513d14692c163a5cd92f58b0280019c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-07 15:41:37 +0530

    Working on JS Logic

--- Script.js
+++ Script.js
@@ -4,25 +4,30 @@ async function fetchData(query) {
 
     let res = await fetch(`${API_URL}s=${query}`);
     let data = await res.json();
+    // const movie=data.Search[0];
     console.log(data);
+
+    // let final=(await fetch(`${API_URL}i=${data.imdbid}`)).json();
+
 }
 
-const button = document.querySelector('.button');
+let idx = 0;
+const button = document.querySelector('.search-icon');
 button.addEventListener('click', () => {
-    let input = document.querySelector('.search-input');
+    let input = document.querySelector('.search-input').value;
     fetchData(input);
 
     const container = document.querySelector('.container');
     container.innerHTML = "";
     container.innerHTML = `
     <div class="left">
-        <img src="" alt="">
+        <img src="${data.Search[idx].Poster}" alt="">
     </div>
     <div class="right">
-        <h1 class="title">The Avengers</h1>
-]       <p>Release Year: </p>
-        <p>Type: </p>
+        <h1 class="title">${data.Search[idx].Title}</h1>
+        <p>Release Year: ${data.Search[idx].Year}</p>
+        <p>Type: ${data.Search[idx].Type}</p>
     </div>`;
-
+    document.querySelector('.box-main').append(container);
 })
-// fetchData('Top Gun: Maverick');
+// fetchData('Inception');
