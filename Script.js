974e0dd68ab35a8ae743063ff83cc3e9fcf2b472
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-07 13:25:43 +0530

    Working on Dynamically Calling API for Data

--- Script.js
+++ Script.js
@@ -1,10 +1,28 @@
-const API_URL=`http://www.omdbapi.com/?apikey=23e5f07d&`;
+const API_URL = `http://www.omdbapi.com/?apikey=23e5f07d&`;
 
-async function fetchData(query){
-    
-    let res=await fetch(`${API_URL}s=${query}`);
-    let data=await res.json();
+async function fetchData(query) {
+
+    let res = await fetch(`${API_URL}s=${query}`);
+    let data = await res.json();
     console.log(data);
 }
 
-fetchData('Top Gun: Maverick');
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {
+    let input = document.querySelector('.search-input');
+    fetchData(input);
+
+    const container = document.querySelector('.container');
+    container.innerHTML = "";
+    container.innerHTML = `
+    <div class="left">
+        <img src="" alt="">
+    </div>
+    <div class="right">
+        <h1 class="title">The Avengers</h1>
+]       <p>Release Year: </p>
+        <p>Type: </p>
+    </div>`;
+
+})
+// fetchData('Top Gun: Maverick');
