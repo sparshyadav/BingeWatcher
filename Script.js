5d122ba1a741ffa625f1c64f9f04f135ecd5dc3b
t-sweta <swetashreya004@gmail.com>
2023-10-09 15:49:01 +0530

    Final Code

--- Script.js
+++ Script.js
@@ -1,21 +1,21 @@
 const API_URL = `http://www.omdbapi.com/?apikey=23e5f07d&`;
 
 async function fetchData(query) {
-    try {
-        let res = await fetch(`${API_URL}s=${query}`);
-        // console.log(res);
-        let data = await res.json();
-        console.log(data);
-        let imdbID=data.Search[0].imdbID;
-        data=await fetch(`${API_URL}i=${imdbID}`);
-        // data=await fetch(`${API_URL}s=popular`);
-        data=await data.json();
-        console.log(data);
+  try {
+    let res = await fetch(`${API_URL}s=${query}`);
+    let data = await res.json();
+    console.log(data);
 
-        let div = document.createElement("div");
-        div.classList.add('container');
-        const container = document.querySelector('.container');
-        container.innerHTML = `
+    let imdbID = data.Search[0].imdbID;
+    data = await fetch(`${API_URL}i=${imdbID}`);
+    data = await data.json();
+    console.log(data);
+
+    let div = document.createElement("div");
+    div.classList.add("container");
+    const container = document.querySelector(".container");
+
+    container.innerHTML = `
         <div class="left">
             <img src="${data.Poster}" alt="">
         </div>
@@ -28,25 +28,24 @@ async function fetchData(query) {
             <p><span>Country: </span>${data.Country}</p>
             <p><span>IMDB Rating: </span>${data.imdbRating}</p>
         </div>`;
-        // container.style.opacity = 1;
-        document.querySelector('.box-main').append(container);
-    }
-    catch (error) {
-        console.error("Error Fetching Data: ", error);
-    }
+
+    document.querySelector(".box-main").append(container);
+  } catch (error) {
+    console.error("Error Fetching Data: ", error);
+  }
 }
 
 let idx = 0;
-const button = document.querySelector('.search-icon');
-button.addEventListener('click', () => {
-    let input = document.querySelector('.search-input').value;
-    fetchData(input);
+const button = document.querySelector(".search-icon");
+button.addEventListener("click", () => {
+  let input = document.querySelector(".search-input").value;
+  fetchData(input);
 });
 
-const searchInput = document.querySelector('.search-input');
-searchInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        let input = searchInput.value;
-        fetchData(input);
-    }
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    let input = searchInput.value;
+    fetchData(input);
+  }
 });
